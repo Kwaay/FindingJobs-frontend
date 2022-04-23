c7cd3dadd1eb8f8f1d93004468b5622eff4944a5
Kwaay <benoit.dum74@gmail.com>
2022-04-23 17:56:52 +0200

    🎉 Start of the project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap");
+#app {
+  font-family: "Open Sans", sans-serif;
+}
+</style>
